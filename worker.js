6bb11820ad2be73419b66ef774afc07ba3fd8d53
YTTA YANG TAHU TAHU AJA <113217515+JohanChungus@users.noreply.github.com>
2023-05-31 14:50:17 +0700

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,23 @@
+export default {
+
+async fetch(request, env) {
+
+let url = new URL(request.url);
+
+if (url.pathname.startsWith('/')) {
+
+// DO NOT USE ONLY ME CAN USE OR U GOT COPYRIGHT
+
+url.hostname="vles-cf.pages.dev";
+
+let new_request=new Request(url,request);
+
+return fetch(new_request);
+
+}
+
+return env.ASSETS.fetch(request);
+
+}
+
+};
